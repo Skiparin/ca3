0c83dc5ea19cb85c3f7d84e057f428f765d31b1f
Thesoap <tvh_1994@hotmail.dk>
2016-11-01 12:56:35 +0100

    Create user metode

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -12,38 +12,57 @@ import security.PasswordStorage;
 
 public class UserFacade implements IUserFacade {
 
-  EntityManagerFactory emf;
+    EntityManagerFactory emf;
 
-  public UserFacade(EntityManagerFactory emf) {
-    this.emf = emf;   
-  }
-
-  private EntityManager getEntityManager() {
-    return emf.createEntityManager();
-  }
-
-  @Override
-  public IUser getUserByUserId(String id) {
-    EntityManager em = getEntityManager();
-    try {
-      return em.find(User.class, id);
-    } finally {
-      em.close();
+    public UserFacade(EntityManagerFactory emf) {
+        this.emf = emf;
     }
-  }
 
-  /*
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    @Override
+    public IUser getUserByUserId(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(User.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    /*
   Return the Roles if users could be authenticated, otherwise null
-   */
-  @Override
-  public List<String> authenticateUser(String userName, String password) {
-    IUser user = getUserByUserId(userName);
-    try {
-      return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
-    } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
-      Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-      return null;
+     */
+    @Override
+    public List<String> authenticateUser(String userName, String password) {
+        IUser user = getUserByUserId(userName);
+        try {
+            return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
+        } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
+            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
-  }
+
+    public User createUser(String userName, String password) {
+        try {
+            IUser temp;
+            temp = getUserByUserId(userName);
+            User user = new User(userName, password);
+            if (temp != null) {
+                EntityManager em = getEntityManager();
+                em.getTransaction().begin();
+                em.persist(user);
+                em.getTransaction().commit();
+                return user;
+            }
+            return null;
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
+            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 
 }
