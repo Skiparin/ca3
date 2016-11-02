ef411b995fbb63013c5015e53198047b7838274f
Skiparin <Og1806x9@hotmail.com>
2016-11-02 14:33:47 +0100

    made changes to controller

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -2,45 +2,62 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'ngAnimate',
-  'angular-jwt',
-  'ui.bootstrap',
-  'myApp.security',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.view3',
-  'myApp.view4',
-  'myApp.view5',
-  'myApp.view6',
-  'myApp.filters',
-  'myApp.directives',
-  'myApp.factories',
-  'myApp.services'
+    'ngRoute',
+    'ngAnimate',
+    'angular-jwt',
+    'ui.bootstrap',
+    'myApp.security',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.view3',
+    'myApp.view4',
+    'myApp.view5',
+    'myApp.view6',
+    'myApp.filters',
+    'myApp.directives',
+    'myApp.factories',
+    'myApp.services'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]).
-config(function ($httpProvider) {
-   $httpProvider.interceptors.push('AuthInterceptor');
-}).
-controller("myCtrl", ["$http", function($http){
-        var self = this;
-        self.user = {userName:"",
-            passwordHash:""}
-        
-        self.create = function (){
-            console.log(self.user.userName + self.user.passwordHash + "fix plx");
-             $http.post('api/demoall/user', self.user)
+        config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.otherwise({redirectTo: '/view1'});
+            }]).
+        config(function ($httpProvider) {
+            $httpProvider.interceptors.push('AuthInterceptor');
+        }).
+        controller("myCtrl", ["$http", "$scope", function ($http, $scope) {
+                var self = this;
+                self.user = {userName: "",
+                    passwordHash: ""}
+
+                self.create = function () {
+                    console.log(self.user.userName + self.user.passwordHash + "fix plx");
+                    $http.post('api/demoall/user', self.user)
                             .success(function (data, status, headers, config) {
                                 console.log("Workingggggggggg");
                             })
                             .error(function (data, status, headers, config) {
                                 console.log("doesn't worrrrkkkkkingggsssss");
                             })
-        };
-        
-        
-}]);
+                };
+
+                self.search = "";
+                self.data = "hey";
+
+                self.getData = function getData() {
+                    $http({url: 'http://cvrapi.dk/api?search=' + self.search + '&country=dk',
+                        skipAuthorization: true,
+                        method: 'GET'
+                    })
+                            .success(function (data, status, headers, config) {
+                                self.data = data;
+                                console.log(data);
+                            })
+                            .error(function (data, status, headers, config) {
+                                console.log("nope");
+                            })
+                };
+
+
+            }]);
 
 
--- src/main/webapp/app/view3/view3.js
+++ src/main/webapp/app/view3/view3.js
@@ -2,29 +2,33 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'app/view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view3', {
+                    templateUrl: 'app/view3/view3.html',
+                    controller: 'View3Ctrl'
+                });
+            }])
 
-.controller('View3Ctrl', function($http,$scope) {
-  $http.get('api/user')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             })
-})
-function getData() {
-  $http.get('http://cvrapi.dk/api?vat=3167%208021&country=dk')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-              console.log(data[0])
-            })
-            .error(function (data, status, headers, config) {
-              console.log("nope")
-             })
-};
+        .controller('View3Ctrl', function ($http, $scope) {
+            var self = this;
+            $http.get('api/user')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
+
+                    })
+
+                    $scope.data = "data";
+
+            self.getData = function getData() {
+                $http.get('http://cvrapi.dk/api?search=Hej&country=dk')
+                        .success(function (data, status, headers, config) {
+                            $scope.data = data;
+                            console.log(data);
+                        })
+                        .error(function (data, status, headers, config) {
+                            console.log("nope");
+                        })
+            };
+        })
