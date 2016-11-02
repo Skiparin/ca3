45714c71c95ff8b41819a5d009358cf3a1df88e1
Oliver <Oliver@10.50.131.96>
2016-11-02 11:39:13 +0100

    stadig html

--- src/main/webapp/app/view5/view5.js
+++ src/main/webapp/app/view5/view5.js
@@ -4,6 +4,17 @@ angular.module('myApp.view5', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view5', {
-    templateUrl: 'app/view5/view5.html'
+    templateUrl: 'app/view5/view5.html',
+    controller: 'View5Ctrl'
   });
-}]);
+}])
+.controller('View5Ctrl', function($http,$scope) {
+  $http.get('api/admin')
+            .success(function (data, status, headers, config) {
+              $scope.data = data;
+            })
+            .error(function (data, status, headers, config) {
+              
+             })
+})
+;
