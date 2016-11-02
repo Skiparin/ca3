5d37aba742e4db1695371a3624ec85601e5c4dfb
Oliver <Oliver@10.50.131.96>
2016-11-02 11:33:56 +0100

    Html bygget videre, top links fungerer på roller. Søgemenu lavet.

--- src/main/webapp/app/view5/view5.js
+++ src/main/webapp/app/view5/view5.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view3', ['ngRoute'])
+angular.module('myApp.view5', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view5', {
