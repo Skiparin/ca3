967174478b3688dbc89d2c3ee38a9a3d1a30755d
michael2750 <2750daugbjerg@gmail.com>
2016-11-04 13:52:58 +0100

    merge

--- src/main/webapp/app/view3/view3.js
+++ src/main/webapp/app/view3/view3.js
@@ -2,60 +2,48 @@
 
 angular.module('myApp.view3', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view3', {
-                    templateUrl: 'app/view3/view3.html',
-                    controller: 'View3Ctrl'
-                });
-            }])
+            $routeProvider.when('/view3', {
+                templateUrl: 'app/view3/view3.html',
+                controller: 'View3Ctrl'
+            });
+        }])
 
-<<<<<<< HEAD
         .controller('View3Ctrl', function ($http, $scope) {
-            var self = this;
-            $http.get('api/user')
-                    .success(function (data, status, headers, config) {
-                        $scope.data = data;
-                    })
-                    .error(function (data, status, headers, config) {
-                    });
-            $scope.data = "data";
-            self.getData = function getData() {
+        var self = this;
+                $http.get('api/user')
+                .success(function (data, status, headers, config) {
+                $scope.data = data;
+                })
+                .error(function (data, status, headers, config) {
+                });
+                $scope.data = "data";
+                self.getData = function getData() {
                 $http.get('http://cvrapi.dk/api?search=Hej&country=dk')
                         .success(function (data, status, headers, config) {
-                            $scope.data = data;
-                            console.log(data);
+                        $scope.data = data;
+                                console.log(data);
                         })
                         .error(function (data, status, headers, config) {
-                            console.log("nope");
+                        console.log("nope");
                         });
-            };
-=======
->>>>>>> origin/database
-            };
-        });
-=======
-        })
-=======
-.controller('View3Ctrl', function($http,$scope, $location) {
-  $http.get('api/user')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
+                }
             })
-            .error(function (data, status, headers, config) {
-              $location('/view1');
-             });
-});
-function getData() {
-  $http.get('http://cvrapi.dk/api?vat=3167%208021&country=dk')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-              console.log(data[0])
-            })
-            .error(function (data, status, headers, config) {
-              console.log("nope")
-             })
-};
-<<<<<<< HEAD
-
-=======
-
->>>>>>> origin/database
+                .controller('View3Ctrl', function($http, $scope, $location) {
+                $http.get('api/user')
+                        .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                        })
+                        .error(function (data, status, headers, config) {
+                        $location('/view1');
+                        });
+                });
+                function getData() {
+                $http.get('http://cvrapi.dk/api?vat=3167%208021&country=dk')
+                        .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                                console.log(data[0])
+                        })
+                        .error(function (data, status, headers, config) {
+                        console.log("nope")
+                        })
+                };
--- src/main/webapp/app/view5/view5.js
+++ src/main/webapp/app/view5/view5.js
@@ -4,9 +4,8 @@ angular.module('myApp.view5', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view5', {
-    templateUrl: 'app/view5/view5.html',
-
-  })
+    templateUrl: 'app/view5/view5.html'
+  });
 }])
  .controller('View5Ctrl', function($http,$scope, $location) {
       var self = this;
