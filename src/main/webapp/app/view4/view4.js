590d8dc383c67014fc65226329e6d798ad5f9286
Thesoap <tvh_1994@hotmail.dk>
2016-11-06 14:50:02 +0100

    test

--- src/main/webapp/app/view4/view4.js
+++ src/main/webapp/app/view4/view4.js
@@ -2,18 +2,29 @@
 
 angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'app/view4/view4.html',
-    controller: 'View4Ctrl'
-  })
-  }])
-  .controller('View4Ctrl', function($http,$scope) {
-  $http.get('api/user')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             })
-});
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view4', {
+                    templateUrl: 'app/view4/view4.html',
+                    controller: 'View4Ctrl'
+                })
+            }])
+        .controller('View4Ctrl', function ($http, $scope) {
+            var self = this;
+            $http.get('api/user')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
+
+                    })
+
+            self.testArray = [{desc: "dkk", rate: 1}, {desc: "euro", rate: 7.44}]
+            self.from;
+            self.to;
+            self.amount;
+
+            self.calc = function () {
+                self.result = self.from / self.to * self.amount;
+                self.result = self.result.toFixed(2);
+            }
+        });
